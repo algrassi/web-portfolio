466750a187da7a9046b2f5279357039b1386eb52
algrassi <ale95.grassi09@gmail.com>
2024-10-15 10:25:35 +0200

    prodution

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  test() {
+    let navMenu = document.getElementById('navMenu');
+    let navBurger = document.getElementById('navBurger');
 
+    let isActiveMenu = navBurger!.classList.contains('is-active');
+
+    if(isActiveMenu) {
+      navMenu!.classList.remove('is-active');
+      navBurger!.classList.remove('is-active');
+    } else {
+      navMenu!.classList.add('is-active');
+      navBurger!.classList.add('is-active');
+    }
+  }
 }
